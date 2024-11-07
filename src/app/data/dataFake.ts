9b2367e253d833e94be70e5fbfa20b56ec025285
Pedro Augusto <pedroaugustogoncalvesribeiro@gmail.com>
2024-11-07 17:58:05 -0300

    Finalizando content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "SAIU NOVA VERSÃO DO ANGULAR",
+        "description": "A nova versão do Angular traz melhorias em performance, ferramentas de desenvolvimento e integração com bibliotecas de terceiros, facilitando a criação de interfaces mais rápidas e dinâmicas. Além disso, aprimora o Angular CLI para fluxos de trabalho otimizados, fortalecendo o framework para aplicações escaláveis e robustas.",
+        "photoCover": "https://th.bing.com/th/id/OIP.Pg3OT493OEFJtAFFbWJC5wHaEC?rs=1&pid=ImgDetMain"
+    },
+    {
+        "id": "2",
+        "title": "Expansão do Uso de IA e Machine Learning no Desenvolvimento de Software",
+        "description": "Nos últimos anos, o uso de Inteligência Artificial (IA) e Machine Learning (ML) tem transformado profundamente o desenvolvimento de software. Essas tecnologias estão sendo integradas de maneira crescente a diversas etapas do ciclo de desenvolvimento, desde a análise e design até a codificação, testes e manutenção. Ferramentas baseadas em IA agora ajudam a identificar padrões em grandes volumes de dados, prever falhas, automatizar tarefas repetitivas e otimizar processos, aumentando a eficiência e precisão dos times de desenvolvimento.",
+        "photoCover": "https://th.bing.com/th/id/OIP.n8U4rv2nBVWgMGXp90lozwHaE8?w=262&h=180&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        "id": "3",
+        "title": "Ascensão de Clientes Automatizados na IoT",
+        "description": "Clientes automatizados são dispositivos inteligentes que podem coletar dados, processá-los e tomar decisões sem intervenção humana, operando de maneira independente ou em colaboração com outros dispositivos conectados. Na IoT, essa automação permite que sensores, atuadores, máquinas e aplicações realizem tarefas complexas, como manutenção preditiva, monitoramento ambiental, controle de qualidade e otimização de recursos energéticos.",
+        "photoCover": "https://th.bing.com/th/id/OIP.MwhkFm34l_NRucCJcVq7rAHaD5?w=305&h=180&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        "id": "4",
+        "title": "Avanços no Desenvolvimento Sustentável de Software",
+        "description": "Os Avanços no Desenvolvimento Sustentável de Software refletem uma mudança essencial para tornar a indústria de tecnologia mais ecologicamente responsável. O desenvolvimento sustentável de software envolve práticas que buscam minimizar o impacto ambiental ao longo de todo o ciclo de vida do software, desde a concepção até a execução e o descarte, levando em conta o consumo de energia, a eficiência dos recursos e a longevidade dos sistemas.",
+        "photoCover": "https://thumbs.dreamstime.com/b/symbolic-depiction-ethical-global-ai-impact-human-ai-figures-holding-globe-ai-ethics-symbolic-depiction-ethical-321252051.jpg"
+    }
+]
